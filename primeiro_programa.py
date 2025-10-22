c3bd0ccaa707a5299d7ca045afc84c21bedd4c1e
Willian Reis <willian.rodriguesreis@gmail.com>
2025-10-22 09:39:21 -0300

    testes1 testes2

--- primeiro_programa.py
+++ primeiro_programa.py
@@ -1,1 +1,2 @@
 print("hello world")
+dir(100)
---
+++ variaveis_constantes.py
@@ -0,0 +1,11 @@
+primeiro_bi = 10
+segundo_bi = 8
+terceiro_bi = 5
+quarto_bi = 7
+
+media = ((primeiro_bi + segundo_bi + terceiro_bi + quarto_bi) / 4)
+
+if (media > 7 ; "aprovado")
+else ("reprovado")
+
+print (media)
