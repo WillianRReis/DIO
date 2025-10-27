7c8a51ced4bb84e1a0500a0bf04738dbc36087b1
Willian Reis <willian.rodriguesreis@gmail.com>
2025-10-27 10:10:01 -0300

    teste3

---
+++ operadores.py
@@ -0,0 +1,9 @@
+produto1 = 10
+produto2 = 20
+
+print (produto1 + produto2 + 3.5)
+
+saldo = 510 // 4
+print (saldo)
+
+print(127*4)
--- variaveis_constantes.py
+++ variaveis_constantes.py
@@ -1,11 +1,17 @@
-primeiro_bi = 10
+primeiro_bi = 8
 segundo_bi = 8
 terceiro_bi = 5
 quarto_bi = 7
 
 media = ((primeiro_bi + segundo_bi + terceiro_bi + quarto_bi) / 4)
 
-if (media > 7 ; "aprovado")
-else ("reprovado")
-
 print (media)
+
+if (media) >= 7:
+
+    print ('aprovado')
+
+else:
+    print ('reprovado')
+    
+
